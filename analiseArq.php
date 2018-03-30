7fdf4da87de9c8e3eb890a407de80dab8a391d11
Stephanie Lima <slrocha17@gmail.com>
2018-03-30 16:39:28 -0300

    gerando complementar

--- analiseArq.php
+++ analiseArq.php
@@ -8,9 +8,6 @@ function eliminarLinha(){
 
 	unset($arquivo[0]);
 
-
-	$tam = sizeof($arquivo);//echo $aux;
-
 	foreach ($arquivo as $key => $value) {
 		$novo = 'newCode.txt';
 		$file = fopen($novo, 'a');
@@ -35,19 +32,52 @@ function separandoSeq(){
 			while ($count != 1264) {
 				$linha = fgetc($newFile);
 				$qntd = strlen(trim($linha));	
-				$escreve = fwrite($fileNew, $linha);	
+				$escreve = fwrite($fileNew, trim($linha));	
 				$count = $qntd + $count;
 				//echo $count."</br>";
 				//break;
 			}		
 		}
 	}
+	fclose($newFile);
 	echo $count;
 	//echo $aux;
 }	
 
-separandoSeq();
+function gerarComplementar(){
+	$newFile = file('seqNova.txt') or die("Error");
+	$complementar = 'complementar.txt';
+	$novo = fopen($complementar, 'a');
 
+	foreach ($newFile as $key => $value) {
+		$stringArray = str_split($value);
+		$tam = count($stringArray);
+
+		print_r($stringArray);
+
+		for($i=0; $i < $tam; $i+=2) {
+	        $aux = $stringArray[$i];
+	        $stringArray[$i] = $stringArray[$i + 1];
+	        $x = $stringArray[$i];
+	        //echo $x;break;
+
+	        $escreve = fwrite($novo, $x);
+	        $escreve_v1 = fwrite($novo, $aux);
+	        //$stringArray[$i + 1] = $aux;
+			//echo $stringArray[$i + 1];break;
+	    }
+	}
+
+	/*for($i = 0; $i < $tam; $i++) {
+		$escreve = fwrite($newFile, $stringArray[$i]);	
+	
+    }  */
+}
+
+
+
+//separandoSeq();
+gerarComplementar();
 
 
 ?>
