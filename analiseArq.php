70ce2df4515fbfc2a7fd99053e11a0073cf6c6b2
Stephanie Lima <slrocha17@gmail.com>
2018-03-30 14:37:36 -0300

    Criando funções para eliminar a primeira linha, gerar um novo arquivo, e sequencia que sera analisada

--- analiseArq.php
+++ analiseArq.php
@@ -1,9 +1,53 @@
 <?php
 
-echo "Hello";
+function eliminarLinha(){
 
-//$arquivo = fopen("Bioinfo_TestSequence_Complete_Genome_FASTA.txt", "r");
+	$arquivo = file("Bioinfo_TestSequence_Complete_Genome_FASTA.txt") or die("Error");
+	$aux = 0;
+	$qntd = 0;
 
-//var_dump($arquivo); 
+	unset($arquivo[0]);
+
+
+	$tam = sizeof($arquivo);//echo $aux;
+
+	foreach ($arquivo as $key => $value) {
+		$novo = 'newCode.txt';
+		$file = fopen($novo, 'a');
+		$writeArq = fwrite($file, $value);	
+	}
+	
+}
+
+function separandoSeq(){
+	$newFile = fopen('newCode.txt', 'r') or die("Error");
+	$aux = 0;
+	$count = 803;
+
+	while (!feof($newFile)) {
+		$linha = fgetc($newFile);
+		$qntd = strlen(trim($linha));	
+		$aux = $qntd + $aux;
+
+		if($aux == 803){			
+			$seqNova = 'seqNova.txt';
+			$fileNew = fopen($seqNova, 'a');
+			while ($count != 1264) {
+				$linha = fgetc($newFile);
+				$qntd = strlen(trim($linha));	
+				$escreve = fwrite($fileNew, $linha);	
+				$count = $qntd + $count;
+				//echo $count."</br>";
+				//break;
+			}		
+		}
+	}
+	echo $count;
+	//echo $aux;
+}	
+
+separandoSeq();
+
+
 
 ?>
