b5bf496a730f702c164baabaa97f9daaaf7528e3
Stephanie Lima <slrocha17@gmail.com>
2018-04-02 20:21:04 -0300

    retirando os prints

--- analiseArq.php
+++ analiseArq.php
@@ -14,15 +14,13 @@ function eliminarLinha(){
 	
 }
 
-function separandoSeq(){
-	
+function separandoSeq(){	
 	$newFile = file_get_contents('newCode.txt')or die("Error");
 	$arqvInvert = strrev($newFile);
 
 	$arquivoInvertido = file_put_contents('arquivoInvertido.txt', $arqvInvert);
 	$fileAberto = fopen('arquivoInvertido.txt', 'r');
 
-	//print_r($arquivoInvertido); 
 	$aux = 0;
 	$count = 803;
 
@@ -39,14 +37,10 @@ function separandoSeq(){
 				$qntd = strlen(trim($linha));	
 				$escreve = fwrite($fileNew, trim($linha));	
 				$count = $qntd + $count;
-				//echo $count."</br>";
-				//break;
 			}		
 		}
 	}
 	fclose($fileAberto);
-	//echo $count;
-	//echo $aux;
 }	
 
 function gerarComplementar(){
@@ -58,27 +52,16 @@ function gerarComplementar(){
 		$stringArray = str_split($value);
 		$tam = sizeof($stringArray);
 
-		//print_r($stringArray);
-
 		for($i=0; $i < $tam; $i+=2) {
 	        $aux = $stringArray[$i];
 	        $stringArray[$i] = $stringArray[$i + 1];
 	        $x = $stringArray[$i];
-	        //echo $x;break;
-
 	        $escreve = fwrite($novo, $x);
 	        $escreve_v1 = fwrite($novo, $aux);
-	        //$stringArray[$i + 1] = $aux;
-			//echo $stringArray[$i + 1];break;
 	    }
 	}
 
 	fclose($novo);
-	
-	/*for($i = 0; $i < $tam; $i++) {
-		$escreve = fwrite($newFile, $stringArray[$i]);	
-	
-    }  */
 }
 
 function separarCodons(){
@@ -89,27 +72,22 @@ function separarCodons(){
 
 	foreach ($novo as $key => $value) {
 		$x = str_split($value);
-		//print_r($x);
 		$tam = sizeof($x); //462
 
 		for ($z=0; $z < $tam; $z+=3) { 
 			$b = $x[$z].$x[$z+1].$x[$z+2];		
 			array_push($codon1, $b);
 		}
-		//print_r($codon1);
 
 		for ($i=1; $i < $tam-2; $i+=3) { //sobram dois
 			$y = $x[$i].$x[$i+1].$x[$i+2];
 			array_push($codon2, $y);
 		}
-		//print_r($codon2);
-
 
 		for ($j=2; $j < $tam-1; $j+=3) { 
 			$a = $x[$j].$x[$j+1].$x[$j+2];
 			array_push($codon3, $a);
 		}
-		//print_r($codon3);
 		gerarFrames($codon1, $codon2, $codon3);
 	}
 }
@@ -119,8 +97,7 @@ function gerarFrames($array1, $array2, $array3){ // incompleto, pensar mais
 	$tamCod2= sizeof($array2);
 	$tamCod3= sizeof($array3);
 	$arrayFrames = array();
-
-	print_r($array1);
+	//print_r($array1);
 
 	for ($i=0; $i < $tamCod1; $i++) { 
 		if ($array1[$i] == 'ATG' || $array1[$i] == 'CTG' || $array1[$i] =='ATT'|| $array1[$i] =='ATA' 
@@ -129,19 +106,16 @@ function gerarFrames($array1, $array2, $array3){ // incompleto, pensar mais
 			print_r($arrayFrames['posicaoInicial']);
 		
 		}
+		
 		if ($array1[$i] == 'TAA' || $array1[$i] == 'TAG' || $array1[$i] == 'TGA'){
-				$arrayFrames['posicaoFinal'] =$i;
-			//print_r($arrayFrames);
+			$arrayFrames['posicaoFinal'] =$i;
 		}	
 	}
-
 }
 
 /*
 falta ver a melhor forma de salvar as posições iniciais e finais, printar em qual frame o gene se encontra, determinar os aminoácidos da proteína codificada, usando a tabela.
 */
-
-
 
 //separarCodons();
 //separandoSeq();
