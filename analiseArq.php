7ba36ac051c9bb53b38aa476e4aa6ea17a4ec00a
Stephanie Lima <slrocha17@gmail.com>
2018-04-07 21:55:00 -0300

    gerando o frame com a formula

--- analiseArq.php
+++ analiseArq.php
@@ -19,12 +19,18 @@ if (!empty($_POST)) {
 	echo "Não houve submit no formulário";
 }
 
+function apagarArquivo($arquivo){
+	if (file_exists($arquivo)) {
+    	unlink($arquivo);
+	} 
+}
+
 function eliminarLinha($posicaoInicial, $posicaoFinal, $fita){
 	$arquivo = file("Bioinfo_TestSequence_Complete_Genome_FASTA.txt") or die("Error");
 	unset($arquivo[0]);
+	$novo = 'newCode.txt';
 
 	foreach ($arquivo as $key => $value) {
-		$novo = 'newCode.txt';
 		$file = fopen($novo, 'a');
 		$writeArq = fwrite($file, $value);	
 	}
@@ -41,7 +47,6 @@ function separandoSeq($posIni, $posFim, $fita){
 		$arquivoInvertido = file_put_contents('arquivoInvertido.txt', $arqvInvert);
 		$fileAberto = fopen('arquivoInvertido.txt', 'r');
 		escreverArquivo($posIni, $posFim, $fileAberto);
-
 	} else {
 		$newFile = fopen('newCode.txt', 'r') or die("Error");
 		escreverArquivo($posIni, $posFim, $newFile);
@@ -51,6 +56,7 @@ function separandoSeq($posIni, $posFim, $fita){
 function escreverArquivo($posIni, $posFim, $file){
 	$aux = 0;
 	$count = $posIni;
+	$seqNova = 'seqNova.txt';
 
 	while (!feof($file)) {
 		$linha = fgetc($file);
@@ -58,7 +64,6 @@ function escreverArquivo($posIni, $posFim, $file){
 		$aux = $qntd + $aux;
 		
 		if($aux == $posIni){			
-			$seqNova = 'seqNova.txt';
 			$fileNew = fopen($seqNova, 'a');
 			while ($count <= $posFim) {
 				$linha = fgetc($file);
@@ -76,7 +81,6 @@ function escreverArquivo($posIni, $posFim, $file){
 function gerarComplementar(){
 
 	$newFile = file('seqNova.txt') or die("Error");
-
 	$complementar = 'complementar.txt';
 	$novo = fopen($complementar, 'a');
 
@@ -106,10 +110,7 @@ function gerarComplementar(){
 	encontrarAminoacido();
 }
 
-function dicionarioAminoacidos($sequencia){
-
-	/*print_r($sequencia);
-	exit();*/
+function dicionarioAminoacidos($sequencia, $posicao){
 
 	$aminoacidos = [];
 	$aminoacidos['TTT'] = 'Fenilanina';
@@ -177,12 +178,15 @@ function dicionarioAminoacidos($sequencia){
 	$aminoacidos['GGA'] = 'Glicina';
 	$aminoacidos['GGG'] = 'Glicina';
 
+	$frame = $posicao - 3 * floor($posicao-1/3);
+	echo "Frame => ". $frame;
+
 	foreach ($sequencia as $key => $value) {
+		echo "Códon =>" . $value . ' - ' ;
+		echo "Aminoácido =>";
 		print_r($aminoacidos[$value]);
 		echo "</br>";
 	}
-
-	
 }
 
 function encontrarAminoacido(){
@@ -191,6 +195,7 @@ function encontrarAminoacido(){
 	$sequenciaCodificada = array();
 	$stopCodon = ['TAA', 'TAG','TGA'];
 	$startCodon = 'ATG';
+	$posicaoInicial = 0;
 
 	foreach ($complementar as $value) {
 		$codons = str_split($value);
@@ -199,12 +204,13 @@ function encontrarAminoacido(){
 		for ($i=0; $i < $tam; $i++) { 
 			$codon = $codons[$i].$codons[$i+1].$codons[$i+2];
 			if ($codon == $startCodon){
+				$posicaoInicial = $i;
 				array_push($sequenciaCodificada, $codon);
 				for ($j=$i +3; $j < $tam; $j+=3) { 
 					$codon = $codons[$j].$codons[$j+1].$codons[$j+2];
 					if(in_array($codon, $stopCodon)){
 						array_push($sequenciaCodificada, $codon);
-						dicionarioAminoacidos($sequenciaCodificada);
+						dicionarioAminoacidos($sequenciaCodificada, $posicaoInicial);
 						exit();
 					} else{
 						array_push($sequenciaCodificada, $codon);
@@ -214,77 +220,5 @@ function encontrarAminoacido(){
 		}
 	}
 }
-
-encontrarAminoacido();
-
-
-
-/*function separarCodons(){
-	$novo = file('complementar.txt');
-	$codon1 = [];
-	$codon2 = [];
-	$codon3 = [];
-
-	foreach ($novo as $value) {
-		$x = str_split($value);
-		$tam = sizeof($x); //462
-
-		for ($z=0; $z < $tam; $z+=3) { 
-			$b = $x[$z].$x[$z+1].$x[$z+2];		
-			array_push($codon1, $b);
-		}
-
-		for ($i=1; $i < $tam-2; $i+=3) { //sobram dois
-			$y = $x[$i].$x[$i+1].$x[$i+2];
-			array_push($codon2, $y);
-		}
-
-		for ($j=2; $j < $tam-1; $j+=3) { 
-			$a = $x[$j].$x[$j+1].$x[$j+2];
-			array_push($codon3, $a);
-		}
-
-		gerarFrames($codon1, $codon2, $codon3);
-	}
-}
-
-function gerarFrames($array1, $array2, $array3){ // incompleto, pensar mais
-	$tamCod1= sizeof($array1);
-	$tamCod2= sizeof($array2);
-	$tamCod3= sizeof($array3);
-	$posicoesIniciais = array();
-	$posicoesFinais = array();
-
-
-	//frame 1
-	for ($i=0; $i < $tamCod1; $i++) { 
-		if ($array1[$i] == 'ATG' || $array1[$i] == 'CTG' || $array1[$i] =='ATT'|| $array1[$i] =='ATA' 
-			|| $array1[$i] =='GTG' || $array1[$i] == 'TTG'){ //considerando que timinia será trocada por uracila, o t será usado
-			$posicoesIniciais = array_push($posicoesIniciais, array("Posicao Inicial" => $array1[$i], "Contador Inicio" => $i, "Frame " => "Frame 1")		
-			);		
-		}
-			print_r($posicoesIniciais);
-
-
-		if ($array1[$i] == 'TAA' || $array1[$i] == 'TAG' || $array1[$i] == 'TGA'){
-			$posicoesFinais = array(
-				array("Posicao Final" => $array1[$i], "Contador Final" => $i, "Frame " => "Frame 1")
-			);		
-		}	
-	}
-
-			print_r($posicoesFinais);
-
-}*/
-
-/*
-o gene codificante tem que ser m
-falta ver a melhor forma de salvar as posições iniciais e finais, printar em qual frame o gene se encontra, determinar os aminoácidos da proteína codificada, usando a tabela.
-*/
-//escolhendoSeq();
-//eliminarLinha();
-//separarCodons();
-//separandoSeq();
-//gerarComplementar();
 
 ?>
